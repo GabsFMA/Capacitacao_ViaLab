ecb323d9424c0053a022f333ea7b910bcc9ed446
GabsFMA <105892017+GabsFMA@users.noreply.github.com>
2024-11-24 10:33:42 -0300

    Conceitos_basicos

---
+++ Atividade 2/Atividade 2.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+##Conversão para numpy arrays 
+arr = np.array([1,2,3])
+##print(arr)
+
+##Converter números para float
+np.float32(345.8)
+
+##Exibindo tipos de dado
+arr.dtype
+
+##Convertendo valores de uma array para float
+np.array([1,2,3], dtype= 'f')
